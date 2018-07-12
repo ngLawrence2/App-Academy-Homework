9a3f831a665533b9345e677055ffe81f43c115c8
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-07-11 21:38:37 -0700

    W4D4

---
+++ W4D4/app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/session/new'
+    mail(to: user.username, subject: 'Welcome to my site')
+
+  end
+end
