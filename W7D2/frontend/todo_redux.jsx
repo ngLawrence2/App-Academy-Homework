471bada15bc2dbb23f3c03d78a004224bf7e3aef
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-07-29 18:22:55 -0700

    W7D2

---
+++ W7D2/frontend/todo_redux.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+   store = applyMiddlewares(store,addLoggingToDispatch);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+
+// const addLoggingToDispatch = (store) => {
+//   let storeDispatch = store.dispatch;
+//   return function(action) {
+//     console.log(store.getState());
+//     console.log(action);
+//     storeDispatch(action);
+//     console.log(store.getState());
+//   }
+// };
+
+
+
+const applyMiddlewares = (store, ...middlewares) => {
+   let dispatch = store.dispatch;
+  middlewares.forEach(function(middleWare) {
+    dispatch = middleWare(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
